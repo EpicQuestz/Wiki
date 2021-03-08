2e3f74741a29479d5d693fd243d36f52d1d4d7f2
StealWonders <6114858+StealWonders@users.noreply.github.com>
2021-03-08 16:37:35 +0100

    Setup a rough draft of how the wiki could be organised.

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,40 @@
 module.exports = {
-  someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
-  },
+  //someSidebar: {
+  //  Docusaurus: ['doc1', 'doc2', 'doc3'],
+  //  Features: ['mdx'],
+  //},
+  categories: [
+    {
+      type: 'category',
+      label: 'General',
+      items: ['doc1', 'doc2']
+    },
+    {
+      type: 'category',
+      label: 'Plugins',
+      items: [
+        'doc3',
+        {
+          WorldEdit: [
+            'doc1',
+            'doc2'
+          ]
+        }
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Building',
+      items: [
+        'doc3',
+        {
+          'Rohan Project': [
+            'doc1',
+            'doc2'
+          ]
+        }
+      ]
+    }
+
+  ]
 };
